58ca4f91d714958706c72a4d47531cea6c93163a
WowDespacito <wangzhi_wow@163.com>
2023-04-26 17:52:51 +0800

    测试

--- test.py
+++ test.py
@@ -1,8 +1,20 @@
-from lstm_genre_classifier_pytorch import LSTM
+import time
 import torch
-state_dict = torch.load('./result/0412_3/model_parameter.pkl')
-MODEL = LSTM(
-    input_dim=33, hidden_dim=128, batch_size=35, output_dim=5, num_layers=2
-)
-MODEL.load_state_dict(state_dict)
-print(MODEL)
+
+# 测试gpu计算耗时
+A = torch.ones(5000, 5000).to('cuda')
+B = torch.ones(5000, 5000).to('cuda')
+startTime2 = time.time()
+for i in range(100):
+    C = torch.matmul(A, B)
+endTime2 = time.time()
+print('gpu计算总时长:', round((endTime2 - startTime2) * 1000, 2), 'ms')
+
+# 测试cpu计算耗时
+A = torch.ones(5000, 5000)
+B = torch.ones(5000, 5000)
+startTime1 = time.time()
+for i in range(100):
+    C = torch.matmul(A, B)
+endTime1 = time.time()
+print('cpu计算总时长:', round((endTime1 - startTime1) * 1000, 2), 'ms')
