ceb42f22a7e78c8bd58e1b071da34f7d6b3828e8
WowDespacito <wangzhi_wow@163.com>
2023-04-26 18:43:46 +0800

    添加双向LSTM

--- train_lstm.py
+++ train_lstm.py
@@ -12,41 +12,10 @@ import torch.nn.functional as F
 from GenreFeatureData import (
     GenreFeatureData,
 )  # local python class with Audio feature extraction (librosa)
+from myModule import LSTM
 
-# class definition
-class LSTM(nn.Module):
-    def __init__(self, input_dim, hidden_dim, batch_size, output_dim=5, num_layers=2):
-        super(LSTM, self).__init__()
-        self.input_dim = input_dim
-        self.hidden_dim = hidden_dim
-        self.batch_size = batch_size
-        self.num_layers = num_layers
 
-        # setup LSTM layer
-        self.lstm = nn.LSTM(self.input_dim, self.hidden_dim, self.num_layers, bidirectional=False)
 
-        # setup output layer
-        self.linear = nn.Linear(self.hidden_dim, output_dim)
-        self.dropout = nn.Dropout()
-
-    def forward(self, input, hidden=None):
-        # lstm step => then ONLY take the sequence's final timetep to pass into the linear/dense layer
-        # Note: lstm_out contains outputs for every step of the sequence we are looping over (for BPTT)
-        # but we just need the output of the last step of the sequence, aka lstm_out[-1]
-        x, hidden = self.lstm(input, hidden)
-        x = self.dropout(x[-1])
-        x = self.linear(x)              # equivalent to return_sequences=False from Keras
-
-        output = F.log_softmax(x, dim=1)
-        return output, hidden
-
-    def get_accuracy(self, logits, target):
-        """ compute accuracy for training round """
-        corrects = (
-                torch.max(logits, 1)[1].view(target.size()).data == target.data
-        ).sum()
-        accuracy = 100.0 * corrects / self.batch_size
-        return accuracy.item()
 
 
 
