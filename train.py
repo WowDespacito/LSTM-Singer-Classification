ba309383cc5f92864de315b0ac9720d090516a5f
WowDespacito <wangzhi_wow@163.com>
2023-04-26 17:13:42 +0800

    双层LSTM模型

--- train.py
+++ train.py
@@ -1,17 +1,6 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-"""
-    PyTorch implementation of a simple 2-layer-deep LSTM for genre classification of musical audio.
-    Feeding the LSTM stack are spectral {centroid, contrast}, chromagram & MFCC features (33 total values)
-
-    Question: Why is there a PyTorch implementation, when we already have Keras/Tensorflow?
-    Answer:   So that we can learn more PyTorch and experiment with modulations on basic
-              architectures within the space of an "easy problem". For example, SRU or SincNets.
-              I'm am also curious about the relative performances of both toolkits.
-
-"""
-
 import os
 
 import matplotlib.pyplot as plt
@@ -34,7 +23,7 @@ class LSTM(nn.Module):
         self.num_layers = num_layers
 
         # setup LSTM layer
-        self.lstm = nn.LSTM(self.input_dim, self.hidden_dim, self.num_layers)
+        self.lstm = nn.LSTM(self.input_dim, self.hidden_dim, self.num_layers, bidirectional=True)
 
         # setup output layer
         self.linear = nn.Linear(self.hidden_dim, output_dim)
@@ -44,15 +33,11 @@ class LSTM(nn.Module):
         # lstm step => then ONLY take the sequence's final timetep to pass into the linear/dense layer
         # Note: lstm_out contains outputs for every step of the sequence we are looping over (for BPTT)
         # but we just need the output of the last step of the sequence, aka lstm_out[-1]
-        # print(input.shape)
-        lstm_out, hidden = self.lstm(input, hidden)
-        # print(hidden)
-        # print(lstm_out.shape)
-        logits = self.linear(lstm_out[-1])              # equivalent to return_sequences=False from Keras
-        # print(logits.shape)
-        logits = self.dropout(logits)
-        genre_scores = F.log_softmax(logits, dim=1)
-        return genre_scores, hidden
+        x, hidden = self.lstm(input, hidden)
+        x = self.linear(x[-1])              # equivalent to return_sequences=False from Keras
+        x = self.dropout(x)
+        output = F.log_softmax(x, dim=1)
+        return output, hidden
 
     def get_accuracy(self, logits, target):
         """ compute accuracy for training round """
