f27ae904c00d256a086abac1bac25b0f636fba04
WowDespacito <wangzhi_wow@163.com>
2023-05-07 18:11:40 +0800

    修正路径错误

--- makeDataSet.py
+++ makeDataSet.py
@@ -24,9 +24,8 @@ for root, dir, files in os.walk('D:\Temporary files\graduateDesign\data'):
     for file in files:
         file_list.append(root + '/' + file)
         label_list.append(file.split('(')[0])
-X_train, X_test, y_train, y_test = train_test_split(file_list, label_list, test_size=0.2)
+X_train, X_test, y_train, y_test = train_test_split(file_list, label_list, test_size=0.3)
 for file in X_train:
-    copyfile(file, "./data/train_data/"+os.path.basename(file))
+    copyfile(file, "../data/train_data/"+os.path.basename(file))
 for file in X_test:
-    copyfile(file, "./data/_test/"+os.path.basename(file))
-    copyfile(file, "./data/validation_data/"+os.path.basename(file))
+    copyfile(file, "../data/validation_data/"+os.path.basename(file))
